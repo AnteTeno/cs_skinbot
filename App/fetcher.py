af7ebe02cd4f6341fb42c38711ba740acbe78bc4
Ante Te <tenoranta@gmail.com>
2025-10-16 10:57:05 +0300

    If i make something out of this im proud

--- App/fetcher.py
+++ App/fetcher.py
@@ -2,3 +2,15 @@ import requests
 from config import CSGOEMPIRE_API_KEY
 
 def fetch_csgoempire():
+    url = "https://csgoempire.com/api/v2/trading/auctions?per_page=10&page=1"
+
+    headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer " + CSGOEMPIRE_API_KEY
+    }
+    response = requests.get(url, headers=headers)
+    print(response.text)
+
+
+if __name__ == "__main__":
+    fetch_csgoempire()
