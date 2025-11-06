227cd6fef20f3d2a6ab1919ac0cf900b6923bbf7
Ante Te <tenoranta@gmail.com>
2025-11-06 15:18:25 +0200

    Starting to work on backend and frontend as seperate. Moved the Application OLD to another folder but still keeping it as a foundation to backend.

--- App/Config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-CSGOEMPIRE_API_KEY = os.getenv("CSGOEMPIRE_API_KEY")
-
-if not CSGOEMPIRE_API_KEY:
-    raise ValueError("CSGOEMPIRE_API_KEY puuttuu .env-tiedostosta!")
